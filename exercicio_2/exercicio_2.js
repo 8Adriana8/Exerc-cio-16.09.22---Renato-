b2996f0405485c5301545783112564217d3eb445
8Adriana8 <acls.records@gmail.com>
2022-09-19 06:51:05 -0300

    pastas adicionadas

---
+++ exercicio_2/exercicio_2.js
@@ -0,0 +1,9 @@
+/** 2. Dado o tamanho do lado de um quadrado, calcular a área e o perímetro do mesmo. */
+
+let lado = Number(prompt("Inserir valor do lado de um quadrado"))
+let calculo = lado * 2
+alert("valor da area do quadrado " + calculo)
+
+calculo = 2 * (base + altura)
+
+alert("valor do perimetro do triangulo: " + calculo)
