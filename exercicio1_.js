629052353324173ea14da435faf7a57ec0924970
8Adriana8 <acls.records@gmail.com>
2022-09-19 06:22:19 -0300

    exercicio 1

---
+++ exercicio1_.js
@@ -0,0 +1,10 @@
+/** Dado o tamanho da base e da altura de um triangulo, calcular a sua área e o seu perímetro  */
+
+let base = Number(prompt("Inserir valor da base do retangulo"))
+let altura = Number(prompt("Inserir o valor da altura do retangulo"))
+let calculo = base * altura
+alert("valor da area do triangulo: " + calculo)
+
+calculo = 2 * (base + altura)
+
+alert("valor do perimetro do triangulo: " + calculo)
